1ac46bf85c806610c07e4cb54b0d5744d806d4b5
tobybrave <leutoby@gmail.com>
2021-08-11 11:52:29 +0100

    removed blog and bookshop project

--- blog/src/services/login.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-const baseUrl = '/api/login'
-
-const login = (credentials) => {
-  const result = axios.post(baseUrl, credentials)
-  return result.then(response => response.data)
-}
-
-export default { login }
