59bb7c7f8a93816a47a121c972852c5544b39e1f
tobybrave <leutoby@gmail.com>
2021-08-11 11:46:07 +0100

    main project

--- blog-api/4.22/index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-const app = require('./app')
-const logger = require('./utils/logger')
-const config = require('./utils/config')
-
-const server = http.createServer(app)
-
-server.listen(config.PORT, () => logger.info(`server is running at PORT ${config.PORT}`))
